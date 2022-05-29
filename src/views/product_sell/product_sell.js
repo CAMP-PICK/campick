66422538ba66e9910c4191fc9f3b043109891781
YONGHWAN <younghwan101@gmail.com>
2022-05-30 04:27:10 +0900

    제품 판매 js 파일 구현 valid error

--- src/views/product_sell/product_sell.js
+++ src/views/product_sell/product_sell.js
@@ -1,26 +1,22 @@
 import * as Api from '../api.js';
 
 // querySelector로 변수 지정
-
-const title = document.querySelector("#titleInput"); //제품명
+const productTitle = document.querySelector("#titleInput"); //제품명
 const categorySelect = document.querySelector("#categorySelectBox"); //카테고리
 const manufacturer = document.querySelector("#manufacturerInput"); //제조사
 const shortDescription = document.querySelector("#shortDescriptionInput"); // 요약 설명
-const detailDescription = document.querySelectordetail("#DescriptionInput"); // 상세 설명
+const detailDescription = document.querySelector("#detailDescriptionInput"); // 상세 설명
 const imagedata = document.querySelector("#imageInput"); // 제품 사진 임시
 const inventory = document.querySelector("#inventoryInput"); // 제품 재고
-const price = document.querySelector("#priceInput"); //제품 가격
-  
+const productPrice = document.querySelector("#priceInput"); //제품 가격
 
-addAllElements();
-addAllEvents();
+const submit = document.querySelector("#submitButton"); //버튼
 
-// html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-async function addAllElements() {}
+currentCategoryList();
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
-    submitButton.addEventListener('click', handleSubmit);
+  submit.addEventListener("click", handleSubmit);
 }
   
   async function currentCategoryList(){
@@ -36,31 +32,34 @@ function addAllEvents() {
       const categoryOption = document.createTextNode(productCategory);
       option.appendChild(categoryOption)
       
-      selectCategory.appendChild(option);
+      categorySelect.appendChild(option);
     }
     return;
   }
-  
+
     async function handleSubmit(e) {
       e.preventDefault();
-    
-      const title = title.value; //제품명
-      const categorySelect = categorySelect.value; //카테고리
-      const manufacturer = manufacturer.value; //제조사
-      const shortDescription = shortDescription.value; // 요약 설명
-      const detailDescription = detailDescription.value; // 상세 설명
-      const imagedata = imagedata.value; // 제품 사진 임시
-      const inventory = inventory.value; // 제품 재고
-      const price = price.value; //제품 가격
+      
+      //추가할 물품 데이터
+      const productTitle = titleInput.value; //제품명
+      const categorySelect = categorySelectBox.value; //카테고리
+      const manufacturer = manufacturerInput.value; //제조사
+      const shortDescription = shortDescriptionInput.value; // 요약 설명
+      const detailDescription = detailDescriptionInput.value; // 상세 설명
+      const imagedata = imageInput.value; // 제품 사진 임시
+      // const inventory = inventory.value; // 제품 재고 router가 없어서 임시로 막아둠
+      const productPrice = priceInput.value; //제품 가격
 
       try{
-        const data = { title, categorySelect, manufacturer, shortDescription, detailDescription }
-        await Api.post(`/api/product_sell/create`, data);
+        const data = {productTitle, productPrice, categorySelect, imagedata, manufacturer, shortDescription, detailDescription};
+        await Api.post(`/api/product/create`, data);
         alert(`정상 등록되었습니다.`);
+
         //선택한 카테고리 페이지로 이동
-        window.location.href(`/${categorySelect}`);
+        window.location.href = `/category/:${productTitle}`;
       } catch(err) {
         alert(`${err.message}`);
       }
     }
-  
+
+addAllEvents();
