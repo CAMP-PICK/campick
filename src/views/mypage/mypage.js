69b39556637f95e95bf19235d6a3f2e679b4dab2
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-05-31 02:47:53 +0900

    개인정보 수정

--- src/views/mypage/mypage.js
+++ src/views/mypage/mypage.js
@@ -1,108 +1,78 @@
 import * as Api from '/api.js';
 import { validateEmail } from '/useful-functions.js';
 
-const testBtn1 = document.querySelector('#testBtn1');
+// 요소모음
 const submitButton = document.querySelector('#submitButton');
-const deleteUserBtn = document.querySelector('#deleteUserBtn');
-
-const deletePassword = document.querySelector('#deletePassword');
-
-const nameInput = document.querySelector('#nameInput');
-const passwordInput = document.querySelector('#passwordInput');
+const fullNameInput = document.querySelector('#fullNameInput');
+const emailInput = document.querySelector('#emailInput');
+const currentPasswordInput = document.querySelector('#currentPasswordInput');
 const newPasswordInput = document.querySelector('#newPasswordInput');
-const confirmNewPasswordInput = document.querySelector(
-  '#confirmNewPasswordInput'
+const newPasswordConfirmInput = document.querySelector(
+  '#newPasswordConfirmInput'
 );
-const telInput = document.querySelector('#telInput');
+const phoneNumberInput = document.querySelector('#phoneNumberInput');
 const addressInput = document.querySelector('#addressInput');
-
-const userInfoEmail = document.querySelector('#userInfoEmail');
-const userInfoName = document.querySelector('#userInfoName');
-const userInfoPhone = document.querySelector('#userInfoPhone');
-const userInfoAddress = document.querySelector('#userInfoAddress');
+const userAddress = document.querySelector('#userAddress');
+const userAddressDetail = document.querySelector('#userAddressDetail');
 
 // addAllEvents();
 
-// function addAllEvents() {
-//   testBtn1.addEventListener('click', logOut);
-//   submitButton.addEventListener('click', setUser);
-//   deleteUserBtn.addEventListener('click', deleteUser);
-// }
-
-// 테스트 api(현재 사용 안함)
-async function userInfo() {
-  const data = await Api.get(`/api/email/${localStorage.getItem('email')}`);
+function addAllEvents() {
+  submitButton.addEventListener('click', setUser);
 }
 
-async function deleteUser(e) {
-  e.preventDefault();
-  const password = deletePassword.value;
-  console.log(password);
-  const email = localStorage.getItem('email');
-  console.log(userinfo);
-  try {
-    const data = { email, password };
-    await Api.post(`/api/userdelete`, data);
-    localStorage.clear();
+// 정보수정 진행
+window.onload = () => {
+  async function setUser(e) {
+    e.preventDefault();
+    const fullName = fullNameInput.value;
+    const currentPasswordInput = currentPasswordInput.value;
+    const newPasswordInput = newPasswordInput.value;
+    const newPasswordConfirmInput = newPasswordConfirmInput.value;
+    const phoneNumberInput = phoneNumberInput.value;
+    const addressInput = addressInput.value;
+    const userAddress = userAddress.value;
+    const userAddressDetail = userAddressDetail.value;
+    const data = await Api.get(`/api/email/${localStorage.getItem('email')}`);
+    const id = data._id;
 
-    alert('회원 탈퇴가 완료 되었습니다.');
+    // 잘 입력했는지 확인
+    const isFullNameValid = fullName.length >= 2;
+    const isEmailValid = validateEmail(email);
+    const isPasswordValid =
+      newPasswordInput.length === 0 || newPasswordInput.length >= 4;
+    const isPasswordConfirm = newPasswordInput === newPasswordConfirmInput;
 
-    // 기본 페이지로 이동
-    window.location.href = '/';
-  } catch (err) {
-    console.error(err.stack);
-    alert(`${err.message}`);
+    if (!isFullNameValid || !isPasswordValid) {
+      return alert('이름은 2글자 이상, 비밀번호는 4글자 이상이어야 합니다.');
+    }
+
+    if (!isEmailValid) {
+      return alert('이메일 형식이 맞지 않습니다.');
+    }
+
+    if (!isPasswordValid) {
+      return alert('새로 변경하시는 비밀번호가 4글자 이상인지 확인해 주세요.');
+    }
+
+    if (!isPasswordConfirm) {
+      return alert('변경하시는 비밀번호와 비밀번호 확인이 일치 하지 않습니다.');
+    }
   }
-}
+};
 
-async function setUser(e) {
-  e.preventDefault();
+// 정보 수정 api 요청
+try {
+  const data = { fullName, newPassword, address, phoneNumber, password };
 
-  const fullName = nameInput.value;
-  const password = passwordInput.value;
-  const newPassword = newPasswordInput.value;
-  const confirmNewPassword = confirmNewPasswordInput.value;
-  const phoneNumber = telInput.value;
-  const address = addressInput.value;
-  const data = await Api.get(`/api/email/${localStorage.getItem('email')}`);
-  const id = data._id;
+  await Api.patch('/api/users', id, data);
 
-  // 잘 입력했는지 확인
-  const isFullNameValid = fullName.length >= 2;
-  const isEmailValid = validateEmail(email);
-  const isPasswordValid = newPassword.length === 0 || newPassword.length >= 4;
-  const isPasswordConfirm = newPassword === confirmNewPassword;
+  alert(`회원님의 정보가 수정 되었습니다.`);
+  // 정보 수정 성공
 
-  if (!isFullNameValid || !isPasswordValid) {
-    return alert('이름은 2글자 이상, 비밀번호는 4글자 이상이어야 합니다.');
-  }
-
-  if (!isEmailValid) {
-    return alert('이메일 형식이 맞지 않습니다.');
-  }
-
-  if (!isPasswordValid) {
-    return alert('새로 변경하시는 비밀번호가 4글자 이상인지 확인해 주세요.');
-  }
-
-  if (!isPasswordConfirm) {
-    return alert('변경하시는 비밀번호와 비밀번호 확인이 일치 하지 않습니다.');
-  }
-
-  // 정보 수정 api 요청
-  try {
-    const data = { fullName, newPassword, address, phoneNumber, password };
-
-    await Api.patch('/api/users', id, data);
-
-    alert(`회원님의 정보가 수정 되었습니다.`);
-
-    // 정보 수정 성공
-
-    // 계정관리 페이지로 이동
-    window.location.href = '/account';
-  } catch (err) {
-    console.error(err.stack);
-    alert(`${err.message}`);
-  }
+  // 계정관리 페이지로 이동
+  window.location.href = '/account';
+} catch (err) {
+  console.error(err.stack);
+  alert(`${err.message}`);
 }
