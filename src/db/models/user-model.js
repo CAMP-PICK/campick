4a8e64d897de07148d13c75ffbfb757eb0ecabd1
jaeyeong815 <jae0ee12@gmail.com>
2022-05-30 13:31:32 +0900

    Revert "Revert "merge""
    This reverts commit cf1dc3d2291fe3fe79f7b480f1a912a40d1b0253.

--- src/db/models/user-model.js
+++ src/db/models/user-model.js
@@ -31,6 +31,11 @@ export class UserModel {
     const updatedUser = await User.findOneAndUpdate(filter, update, option);
     return updatedUser;
   }
+
+  async delete(email) {
+    const user = await User.deleteOne({ email: email })
+    return user;
+  }
 }
 
 const userModel = new UserModel();
---
+++ src/db/schemas/product-schema.js
@@ -0,0 +1,36 @@
+import { Schema } from 'mongoose';
+
+const ProductSchema = new Schema(
+  {
+    productName: {
+      type: String,
+      required: true,
+    },
+    productPrice: {
+      type: Number,
+      required: true,
+    },
+    productImage: {
+      type: String,
+      required: false,
+    },
+    productManuf: {
+      type: String,
+      required: true,
+    },
+    productShortDes: {
+      type: String,
+      required: true,
+    },
+    productLongDes: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    collection: 'products',
+    timestamps: true,
+  }
+);
+
+export { ProductSchema };
