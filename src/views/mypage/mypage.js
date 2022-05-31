1217ddc95ae35f4f80b86e231ed039e8d981eaaf
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-05-31 11:06:03 +0900

    사용자 정보수정 구현중(아직완료안됨)

--- src/views/mypage/mypage.js
+++ src/views/mypage/mypage.js
@@ -15,51 +15,50 @@ const addressInput = document.querySelector('#addressInput');
 const userAddress = document.querySelector('#userAddress');
 const userAddressDetail = document.querySelector('#userAddressDetail');
 
-// addAllEvents();
+addAllEvents();
 
 function addAllEvents() {
   submitButton.addEventListener('click', setUser);
 }
 
 // 정보수정 진행
-window.onload = () => {
-  async function setUser(e) {
-    e.preventDefault();
-    const fullName = fullNameInput.value;
-    const currentPasswordInput = currentPasswordInput.value;
-    const newPasswordInput = newPasswordInput.value;
-    const newPasswordConfirmInput = newPasswordConfirmInput.value;
-    const phoneNumberInput = phoneNumberInput.value;
-    const addressInput = addressInput.value;
-    const userAddress = userAddress.value;
-    const userAddressDetail = userAddressDetail.value;
-    const data = await Api.get(`/api/email/${localStorage.getItem('email')}`);
-    const id = data._id;
 
-    // 잘 입력했는지 확인
-    const isFullNameValid = fullName.length >= 2;
-    const isEmailValid = validateEmail(email);
-    const isPasswordValid =
-      newPasswordInput.length === 0 || newPasswordInput.length >= 4;
-    const isPasswordConfirm = newPasswordInput === newPasswordConfirmInput;
+async function setUser(e) {
+  e.preventDefault();
+  const fullNameInput = fullNameInput.value;
+  const currentPasswordInput = currentPasswordInput.value;
+  const newPasswordInput = newPasswordInput.value;
+  const newPasswordConfirmInput = newPasswordConfirmInput.value;
+  const phoneNumberInput = phoneNumberInput.value;
+  const addressInput = addressInput.value;
+  const userAddress = userAddress.value;
+  const userAddressDetail = userAddressDetail.value;
+  const data = await Api.get(`/api/email/${localStorage.getItem('email')}`);
+  const id = data._id;
 
-    if (!isFullNameValid || !isPasswordValid) {
-      return alert('이름은 2글자 이상, 비밀번호는 4글자 이상이어야 합니다.');
-    }
+  // 잘 입력했는지 확인
+  const isFullNameValid = fullNameInput.length >= 2;
+  const isEmailValid = validateEmail(email);
+  const isPasswordValid =
+    newPasswordInput.length === 0 || newPasswordInput.length >= 4;
+  const isPasswordConfirm = newPasswordInput === newPasswordConfirmInput;
 
-    if (!isEmailValid) {
-      return alert('이메일 형식이 맞지 않습니다.');
-    }
-
-    if (!isPasswordValid) {
-      return alert('새로 변경하시는 비밀번호가 4글자 이상인지 확인해 주세요.');
-    }
-
-    if (!isPasswordConfirm) {
-      return alert('변경하시는 비밀번호와 비밀번호 확인이 일치 하지 않습니다.');
-    }
+  if (!isFullNameValid || !isPasswordValid) {
+    return alert('이름은 2글자 이상, 비밀번호는 4글자 이상이어야 합니다.');
   }
-};
+
+  if (!isEmailValid) {
+    return alert('이메일 형식이 맞지 않습니다.');
+  }
+
+  if (!isPasswordValid) {
+    return alert('새로 변경하시는 비밀번호가 4글자 이상인지 확인해 주세요.');
+  }
+
+  if (!isPasswordConfirm) {
+    return alert('변경하시는 비밀번호와 비밀번호 확인이 일치 하지 않습니다.');
+  }
+}
 
 // 정보 수정 api 요청
 try {
@@ -76,3 +75,4 @@ try {
   console.error(err.stack);
   alert(`${err.message}`);
 }
+
