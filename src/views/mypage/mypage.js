0b7fc80ff5afbc9e50033e3b085bba8fdf0b403d
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-06-01 00:46:57 +0900

    정보수정페이지 구현

--- src/views/mypage/mypage.js
+++ src/views/mypage/mypage.js
@@ -1,5 +1,4 @@
 import * as Api from '/api.js';
-import { validateEmail } from '/useful-functions.js';
 
 // 요소모음
 const submitButton = document.querySelector('#submitButton');
@@ -11,44 +10,57 @@ const newPasswordConfirmInput = document.querySelector(
   '#newPasswordConfirmInput'
 );
 const phoneNumberInput = document.querySelector('#phoneNumberInput');
-const addressInput = document.querySelector('#addressInput');
-const userAddress = document.querySelector('#userAddress');
-const userAddressDetail = document.querySelector('#userAddressDetail');
 
-addAllEvents();
+// const addressInput = document.querySelector('#addressInput');
+// const userAddress = document.querySelector('#userAddress');
+// const userAddressDetail = document.querySelector('#userAddressDetail');
 
-function addAllEvents() {
-  submitButton.addEventListener('click', setUser);
-}
+// 현재 유저 객체
+let user = {};
+
+const init = () => {
+  fetchUserInfo();
+  addEvent();
+};
+
+const addEvent = () => {
+  // 정보 수정 버튼을 누르면 updateUserInfo 함수 실행
+  submitButton.addEventListener('click', updateUserInfo);
+};
+
+const setUserInfo = (user) => {
+  fullNameInput.value = user.fullName;
+  emailInput.value = user.email;
+  phoneNumberInput.value = user.phoneNumber;
+};
+
+const fetchUserInfo = async () => {
+  const email = localStorage.getItem('email');
+  try {
+    user = await Api.get(`/api/email/${email}`);
+    setUserInfo(user);
+  } catch (err) {
+    alert(`${err.message}`);
+  }
+};
 
 // 정보수정 진행
-
-async function setUser(e) {
+const updateUserInfo = (e) => {
   e.preventDefault();
-  const fullNameInput = fullNameInput.value;
-  const currentPasswordInput = currentPasswordInput.value;
-  const newPasswordInput = newPasswordInput.value;
-  const newPasswordConfirmInput = newPasswordConfirmInput.value;
-  const phoneNumberInput = phoneNumberInput.value;
-  const addressInput = addressInput.value;
-  const userAddress = userAddress.value;
-  const userAddressDetail = userAddressDetail.value;
-  const data = await Api.get(`/api/email/${localStorage.getItem('email')}`);
-  const id = data._id;
+  // input에 있는 각각의 값을 받아온다.
+  const fullName = fullNameInput.value;
+  const currentPassword = currentPasswordInput.value;
+  const newPassword = newPasswordInput.value;
+  const newPasswordConfirm = newPasswordConfirmInput.value;
+  const phoneNumber = phoneNumberInput.value;
 
-  // 잘 입력했는지 확인
-  const isFullNameValid = fullNameInput.length >= 2;
-  const isEmailValid = validateEmail(email);
-  const isPasswordValid =
-    newPasswordInput.length === 0 || newPasswordInput.length >= 4;
-  const isPasswordConfirm = newPasswordInput === newPasswordConfirmInput;
+  // 인풋 유효성 검사
+  const isFullNameValid = fullName.length >= 2;
+  const isPasswordValid = newPassword.length === 0 || newPassword.length >= 4;
+  const isPasswordConfirm = newPassword === newPasswordConfirm;
 
   if (!isFullNameValid || !isPasswordValid) {
     return alert('이름은 2글자 이상, 비밀번호는 4글자 이상이어야 합니다.');
-  }
-
-  if (!isEmailValid) {
-    return alert('이메일 형식이 맞지 않습니다.');
   }
 
   if (!isPasswordValid) {
@@ -58,21 +70,29 @@ async function setUser(e) {
   if (!isPasswordConfirm) {
     return alert('변경하시는 비밀번호와 비밀번호 확인이 일치 하지 않습니다.');
   }
-}
 
-// 정보 수정 api 요청
-try {
-  const data = { fullName, newPassword, address, phoneNumber, password };
+  // 변경되는 유저 정보 취합
+  const userInfo = {
+    fullName,
+    password: currentPassword,
+    newPassword,
+    address: '',
+    phoneNumber,
+  };
 
-  await Api.patch('/api/users', id, data);
+  // 유저 정보를 수정하는 api 요청
+  patchUserInfo(userInfo);
+};
 
-  alert(`회원님의 정보가 수정 되었습니다.`);
-  // 정보 수정 성공
+// 유저 정보 수정 api
+const patchUserInfo = async (userInfo) => {
+  try {
+    await Api.patch(`/api/users`, user._id, userInfo);
+    alert('정상적으로 수정되었습니다.');
+    window.location.href = '/account';
+  } catch (err) {
+    alert(`${err.message}`);
+  }
+};
 
-  // 계정관리 페이지로 이동
-  window.location.href = '/account';
-} catch (err) {
-  console.error(err.stack);
-  alert(`${err.message}`);
-}
-
+init();
