60ad700a25e8b3d6246054cf885809e753039853
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-06-01 04:20:41 +0900

    회원 탈퇴 기능 구현

--- src/views/account/account.js
+++ src/views/account/account.js
@@ -107,66 +107,66 @@ if (data.role == 'manager-user') {
           <p class="subtitle">모든 정보를 안전하게 삭제한 후 탈퇴할 수 있습니다.</p>
         </article>
       </div>`
-    );
-  }
-  const logoutBtn = document.querySelector('#logoutBtn');
-  
-  const showBtn = document.querySelector('#showModal');
-  const modalDlg = document.querySelector('#image-modal');
-  const imageModalCloseBtn = document.querySelector('#image-modal-close');
-  const deletePassword = document.querySelector('#deletePassword');
-  const deleteUserBtn = document.querySelector('#deleteUserBtn')
-  
-  addAllEvents();
-  
-  function addAllEvents() {
-    if (logoutBtn) logoutBtn.addEventListener('click', logOut);
-    imageModalCloseBtn.addEventListener('click', closeModal)
-    showBtn.addEventListener('click', openModal)
-    deleteUserBtn.addEventListener('click', deleteUser)
-  }
-  
-  function openModal() {
-    modalDlg.classList.add('is-active');
-  };
-  
-  function closeModal() {
-    modalDlg.classList.remove('is-active');
-  };
-  
-  async function deleteUser(e) {
-    e.preventDefault();
-    const email = localStorage.getItem('email');
-    const password = deletePassword.value;
-  
-    try {
-      const data = { email, password }
-      await Api.post('/api/userdelete', data);
-      localStorage.clear()
-  
-      alert('회원 탈퇴가 완료 되었습니다.')
-  
-      // 기본 페이지로 이동
-      window.location.href = '/';
-    } catch (err) {
-      console.error(err.stack);
-      alert(`${err.message}`);
-    }
-  }
-  
-  async function logOut(e) {
-    e.preventDefault();
-    try {
-      localStorage.clear();
-  
-      alert('로그아웃이 완료 되었습니다.');
-  
-      // 기본 페이지로 이동
-      window.location.href = '/';
-    } catch (err) {
-      console.error(err.stack);
-      alert(`${err.message}`);
-      ``;
-    }
-  }
+  );
+}
 
+const logoutBtn = document.querySelector('#logoutBtn');
+
+const showBtn = document.querySelector('#showModal');
+const modalDlg = document.querySelector('#image-modal');
+const imageModalCloseBtn = document.querySelector('#image-modal-close');
+const deletePassword = document.querySelector('#deletePassword');
+const deleteUserBtn = document.querySelector('#deleteUserBtn');
+
+addAllEvents();
+
+function addAllEvents() {
+  if (logoutBtn) logoutBtn.addEventListener('click', logOut);
+  imageModalCloseBtn.addEventListener('click', closeModal);
+  showBtn.addEventListener('click', openModal);
+  deleteUserBtn.addEventListener('click', deleteUser);
+}
+
+function openModal() {
+  modalDlg.classList.add('is-active');
+}
+
+function closeModal() {
+  modalDlg.classList.remove('is-active');
+}
+
+async function deleteUser(e) {
+  e.preventDefault();
+  const email = localStorage.getItem('email');
+  const password = deletePassword.value;
+
+  try {
+    const data = { email, password };
+    await Api.post('/api/userdelete', data);
+    localStorage.clear();
+
+    alert('회원 탈퇴가 완료 되었습니다.');
+
+    // 기본 페이지로 이동
+    window.location.href = '/';
+  } catch (err) {
+    console.error(err.stack);
+    alert(`${err.message}`);
+  }
+}
+
+async function logOut(e) {
+  e.preventDefault();
+  try {
+    localStorage.clear();
+
+    alert('로그아웃이 완료 되었습니다.');
+
+    // 기본 페이지로 이동
+    window.location.href = '/';
+  } catch (err) {
+    console.error(err.stack);
+    alert(`${err.message}`);
+    ``;
+  }
+}
