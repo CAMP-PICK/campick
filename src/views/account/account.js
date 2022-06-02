66d3fed6e5ac76568fd7c5f8150755a26e0664a2
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-06-02 18:48:21 +0900

    account 페이지 로그아웃 버그

--- src/views/account/account.js
+++ src/views/account/account.js
@@ -155,18 +155,3 @@ async function deleteUser(e) {
   }
 }
 
-async function logOut(e) {
-  e.preventDefault();
-  try {
-    localStorage.clear();
-
-    alert('로그아웃이 완료 되었습니다.');
-
-    // 기본 페이지로 이동
-    window.location.href = '/';
-  } catch (err) {
-    console.error(err.stack);
-    alert(`${err.message}`);
-    ``;
-  }
-}
