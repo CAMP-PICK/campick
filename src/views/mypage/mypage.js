5c75d2d8eb34a9829b3a70f7cfa21732c27a4569
computer <scrway15@gmail.com>
2022-06-03 18:12:21 +0900

    유저 정보 수정페이지에서 만약  db에 저장된 회원 정보가 있다면 불러오기

--- src/views/mypage/mypage.js
+++ src/views/mypage/mypage.js
@@ -30,10 +30,10 @@ const addEvent = () => {
 const setUserInfo = (user) => {
   fullNameInput.value = user.fullName;
   emailInput.value = user.email;
-  // phoneNumberInput.value = user.phoneNumber;
-  // userAddressInput.value = user.userAddress;
-  // userAddressDetailInput.value = user.userAddressDetail;
-  // addressInput.value = user.addressCode;
+  phoneNumberInput.value = user.phoneNumber;
+  userAddressInput.value = user.address.address1;
+  userAddressDetailInput.value = user.address.address2;
+  addressInput.value = user.address.address3;
 };
 
 const fetchUserInfo = async () => {
