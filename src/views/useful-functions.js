5d7f41259b069a382f996a12117690000d364182
yeonju <97602748+yeondooo@users.noreply.github.com>
2022-06-04 01:06:06 +0900

    css 수정

--- src/views/useful-functions.js
+++ src/views/useful-functions.js
@@ -236,10 +236,10 @@ export const sortBtn = () => {
   section.insertAdjacentHTML(
     `afterbegin`,
     `<div class="product-sort">
-  <button class="button is-small sort-btn name-sort" style="font-family: 'Noto Sans KR', sans-serif">이름순 정렬</button>
-  <button class="button is-small sort-btn price-sort" style="font-family: 'Noto Sans KR', sans-serif">가격순 정렬</button>
+  <button class="button is-small sort-btn name-sort" style="font-family: 'Noto Sans KR', sans-serif"><b>이름순 정렬</b></button>
+  <button class="button is-small sort-btn price-sort" style="font-family: 'Noto Sans KR', sans-serif"><b>가격순 정렬</b></button>
   <button class="button is-small sort-btn newest-sort" style="font-family: 'Noto Sans KR', sans-serif">
-    최신순 정렬
+    <b>최신순 정렬</b>
   </button>
 </div>`
   );
