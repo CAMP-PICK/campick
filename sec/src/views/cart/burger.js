2cf5a9bfcd8b0d9b4017bc324d8d44fa80db0eb0
jaeyeong815 <jae0ee@naver.com>
2022-07-26 20:07:59 +0900

    style: file delete

--- sec/src/views/cart/burger.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  const $burger = document.querySelector('.navbar-burger');
-  if (!$burger) return;
-  $burger.addEventListener('click', (e) => {
-    let $dropMenu = document.querySelector('.navbar-menu');
-    $burger.classList.toggle('is-active');
-    $dropMenu.classList.toggle('is-active');
-  });
-})();
